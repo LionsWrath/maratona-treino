682854647065112affdc48e466b18602429b7569
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2017-09-27 16:36:19 -0300

    Update gitignored files

--- Maratona2016/huaauhahhuahau/testSingle.sh
+++
@@ -1,1 +0,0 @@
-time ./a.out < ../input-output/H/input/H_${1} && cat ../input-output/H/output/H_${1}
--- Maratona2016/ladrilhos/testSingle.sh
+++
@@ -1,1 +0,0 @@
-time ./a.out < ../input-output/L/input/L_${1} && cat ../input-output/L/output/L_${1}
